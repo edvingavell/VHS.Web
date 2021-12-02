8ef71896049a2d871272020f18cf6333dc094a54
edvingavell <82473656+edvingavell@users.noreply.github.com>
2021-12-02 15:03:37 +0100

    Delete ExpressDb.cs

--- VHS.Core/ExpressDb.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace VHS.Core
-{
-    public class ExpressDb
-    {
-        public static readonly string ConnectionString = "Server=E-GAMING\\SQLEXPRESS;Initial Catalog=VHSDb;UID=sa;Password=qwerty123";
-    }
-}
